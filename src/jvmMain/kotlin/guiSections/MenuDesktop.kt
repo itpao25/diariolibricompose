142f99d07ade74bad00d3db29236f39bd29d0ecf
Paolo <tropaolo@gmail.com>
2022-12-23 22:48:48 +0100

    Menù draggable

--- src/jvmMain/kotlin/guiSections/MenuDesktop.kt
+++ src/jvmMain/kotlin/guiSections/MenuDesktop.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import java.awt.Cursor
 
@@ -28,7 +30,6 @@ fun menuDesktop() {
     return Box(
         modifier = Modifier.width(sizeMenuHorizontal).fillMaxHeight()
     ) {
-
         PermanentNavigationDrawer(
             modifier = Modifier
                 .background(color = Color(0xFF424242))
@@ -39,16 +40,21 @@ fun menuDesktop() {
                     item()
                 }
             }
-        ) {}
+        ) {
+        }
 
+        val density = LocalDensity.current;
         Box(
             modifier = Modifier.align(Alignment.TopEnd)
         ) {
-            menuDesktopResizer(onResize =
-                rememberDraggableState { delta ->
-                    val posHorizontal = sizeMenuHorizontal + Dp(delta)
-                    if (posHorizontal <= maxWidthMenu && posHorizontal > 100.dp) {
-                        sizeMenuHorizontal += Dp(delta);
+            menuDesktopResizer(
+                onResize = rememberDraggableState {
+                    val dp = with(density) {
+                        it.toDp();
+                    }
+                    val newWidth = sizeMenuHorizontal + dp;
+                    if(newWidth <= maxWidthMenu && newWidth >= 100.dp) {
+                        sizeMenuHorizontal += dp;
                     }
                 }
             )
